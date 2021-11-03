ba016786d70f73f38d842b24b187abb57f32a552
BEN-SAID WALID <walid.ben-said@etu.unistra.fr>
2021-11-03 17:25:01 +0100

    Début Exercice 4

--- TP2/exercice4.py
+++ TP2/exercice4.py
@@ -10,6 +10,24 @@ def display(x):
         n = n+1
     dst = cv.GaussianBlur(img,(n,n),s)
     final = np.concatenate((img,dst),axis=0)
+    ####################
+    ## [ -1 , 0 , 1 ] ##
+    ## [ -2 , 0 , 2 ] ##
+    ## [ -1 , 0 , 1 ] ##
+    ####################
+    sobelx64f = cv.Sobel(img,cv.CV_64F,1,0)
+    abs_sobelx64f = np.absolute(sobelx64f)
+    sobelx_8u = np.uint8(abs_sobelx64f)
+
+    ######################
+    ## [ -1 , -2 , -1 ] ##
+    ## [  0 ,  0 ,  0 ] ##
+    ## [  1 ,  2 ,  1 ] ##
+    ######################
+    sobely64f = cv.Sobel(img,cv.CV_64F,0,1)
+    abs_sobely64f = np.absolute(sobely64f)
+    sobely_8u = np.uint8(abs_sobely64f)
+
     cv.imshow(window_title,final)
 
 if len(sys.argv) != 2:
@@ -19,21 +37,7 @@ img = cv.imread(cv.samples.findFile(sys.argv[1]),cv.IMREAD_COLOR)
 if img is None:
     sys.exit("Could not read the image.")
 
-####################
-## [ -1 , 0 , 1 ] ##
-## [ -2 , 0 , 2 ] ##
-## [ -1 , 0 , 1 ] ##
-####################
-sobelX = np.array([[-1,0,1],[-2,0,2],[-1,0,1]])
-
-######################
-## [ -1 , -2 , -1 ] ##
-## [  0 ,  0 ,  0 ] ##
-## [  1 ,  2 ,  1 ] ##
-######################
-sobelY = np.array([[-1,-2,-1],[0,0,0],[1,2,1]])
-
 cv.namedWindow(window_title)
-cv.createTrackbar("Sigma",window_title,0,20,display)
+cv.createTrackbar("Sigma",window_title,1,10,display)
 display("initialisation")
 cv.waitKey(0)
