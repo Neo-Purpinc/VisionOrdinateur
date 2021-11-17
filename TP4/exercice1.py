d1edcb43524b8e9494133b80a7e0887c1235ee45
BEN-SAID WALID <walid.ben-said@etu.unistra.fr>
2021-11-17 23:12:37 +0100

    Exercice 1 DONE

---
+++ TP4/exercice1.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+import sys
+import numpy as np
+# Verify that the user has provided a filename, x and y
+if len(sys.argv) != 4:
+    print("Usage: python3 exercice1.py <filename> <x> <y>")
+    sys.exit(1)
+
+img = cv.imread(cv.samples.findFile(sys.argv[1]),cv.IMREAD_COLOR)
+x = sys.argv[2]
+y = sys.argv[3]
+rows,cols = img.shape[0:2]
+M = np.float32([[1,0,x],[0,1,y]])
+dst = cv.warpAffine(img,M,(cols,rows))
+res = np.concatenate((img,dst),axis=1)
+cv.imshow('Original',res)
+cv.waitKey(0)
