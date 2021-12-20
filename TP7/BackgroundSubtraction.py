84284c70d4fe1ffb04b2d730d013780f8aa9705c
BEN-SAID WALID <walid.ben-said@etu.unistra.fr>
2021-12-20 11:39:13 +0100

    Background Sub v1 (OpenCV & My)

--- TP7/BackgroundSubtraction.py
+++ TP7/BackgroundSubtraction.py
@@ -1,23 +1,33 @@
 import cv2 as cv
 import numpy as np
 import sys
+import argparse
 
-if len(sys.argv) != 3:
-    print("Usage: python3 BackgroundSubtraction.py <videoPath> <BackgroundImagePath>")
-    sys.exit(1)
-video = cv.VideoCapture(sys.argv[1])
-background = cv.imread(sys.argv[2],cv.IMREAD_COLOR)
-bgSub = cv.createBackgroundSubtractorMOG2()
-kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE,(10,10))
+parser = argparse.ArgumentParser(description='This program use background subtraction methods provided by \
+                                              OpenCV.')
+parser.add_argument('--input', type=str, help='Path to a video.', default='../Videos/video1.avi')
+parser.add_argument('--background',type=str,help="Path to the output image file.", default='../Images/times_square.jpg')
+parser.add_argument('--algo', type=str, help='Background subtraction method (KNN, MOG2).', default='MOG2')
+args = parser.parse_args()
+
+video = cv.VideoCapture(args.input)
+background = cv.imread(args.output,cv.IMREAD_COLOR)
+if args.algo == 'MOG2':
+    bgSub = cv.createBackgroundSubtractorMOG2()
+else:
+    bgSub = cv.createBackgroundSubtractorKNN()
+
+kernel = np.ones((5,5),np.uint8)
 while(video.isOpened()):
     ret,frame_o = video.read()
     if ret is True:
         frame = cv.resize(frame_o,(800,500))
-        mask = cv.morphologyEx(cv.morphologyEx(bgSub.apply(frame), cv.MORPH_OPEN, kernel),cv.MORPH_CLOSE,kernel)
+        frame = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+        mask = cv.morphologyEx(cv.morphologyEx(bgSub.apply(frame), cv.MORPH_OPEN, kernel,iterations=2),cv.MORPH_CLOSE,kernel,iterations=2)
         res= cv.resize(background,(frame.shape[1], frame.shape[0]))
         res[mask==255] = frame[mask==255]
-        cv.imshow('Mask',mask)
-        cv.imshow('Frame',frame)
+        # cv.imshow('Mask',mask)
+        # cv.imshow('Frame',frame)
         cv.imshow('Res',res)
         if cv.waitKey(30) & 0xff == 27:
             break
--- TP7/MyBackgroundSubtraction.py
+++ TP7/MyBackgroundSubtraction.py
@@ -1,11 +1,39 @@
 import cv2 as cv
 import numpy as np
 import sys
+import argparse
+# import pafy
 
-if len(sys.argv) != 3:
-    print("Usage: python3 MyBackgroundSubtraction.py <videoPath> <BackgroundImagePath>")
-    sys.exit(1)
-video = cv.VideoCapture(sys.argv[1])
-background = cv.imread(sys.argv[2],cv.IMREAD_COLOR)
-bgSub = cv.createBackgroundSubtractorMOG2()
+parser = argparse.ArgumentParser(description='This program use background subtraction implemented by Walid BEN SAID.')
+parser.add_argument('--input', type=str, help='Path to a video.', default='../Videos/video1.avi')
+parser.add_argument('--background',type=str,help="Path to the output image file.", default='../Images/times_square.jpg')
+args = parser.parse_args()
+
+# url = "https://www.youtube.com/watch?v=EBhCrTPpdBI"
+# video1 = pafy.new(url)
+# best = video1.getbest(preftype="mp4")
+# video = cv.VideoCapture(best.url)
+video = cv.VideoCapture(args.input)
+background = cv.imread(args.background,cv.IMREAD_COLOR)
 kernel = np.ones((5,5),np.uint8)
+
+re,current_frame = video.read()
+previous_frame = current_frame
+while(video.isOpened()):
+    current_frame_gray = cv.cvtColor(current_frame,cv.COLOR_BGR2GRAY)
+    previous_frame_gray = cv.cvtColor(previous_frame,cv.COLOR_BGR2GRAY)
+
+    mask = cv.absdiff(current_frame_gray,previous_frame_gray)
+    th, binary_mask = cv.threshold(mask,20,255,cv.THRESH_BINARY)
+    binery_mask = cv.morphologyEx(cv.morphologyEx(binary_mask, cv.MORPH_OPEN, kernel,iterations=2),cv.MORPH_CLOSE,kernel,iterations=2)
+    res = cv.resize(background,(current_frame.shape[1], current_frame.shape[0]))
+    res[binary_mask==255] = current_frame[binary_mask==255]
+    # cv.imshow('Mask',binary_mask)
+    # cv.imshow('Frame',current_frame)
+    cv.imshow('Res',res)
+    if cv.waitKey(30) & 0xff == 27:
+        break
+    previous_frame = current_frame.copy()
+    re,current_frame = video.read()
+video.release()
+cv.destroyAllWindows()
